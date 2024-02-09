7e1193077c4fda1c2f643a63e3c20a5353d9e2b1
DanielLopezUCM <117196575+DanielLopezUCM@users.noreply.github.com>
2024-02-09 10:50:27 +0100

    Actualizadas las plantillas

--- Plantillas/NumeroCasos.cpp
+++ Plantillas/NumeroCasos.cpp
@@ -6,43 +6,47 @@
 
 #include <iostream>
 #include <fstream>
-#include <...>
+#include <vector>
 
-#include "..."  // propios o los de las estructuras de datos de clase
+using namespace std;
 
 // función que resuelve el problema
 // comentario sobre el coste, O(f(N)), donde N es ...
-Solucion resolver(Datos datos) {
-   ...
+Solucion resolver(Datos datos)
+{
 }
 
 // resuelve un caso de prueba, leyendo de la entrada la
 // configuración, y escribiendo la respuesta
-void resuelveCaso() {
-   
+void resuelveCaso()
+{
+
    // leer los datos de la entrada
-   
+
    Solucion sol = resolver(datos);
-   
+
    // escribir solución
 }
 
-int main() {
+int main()
+{
    // ajustes para que cin extraiga directamente de un fichero
 #ifndef DOMJUDGE
    std::ifstream in("casos.txt");
    auto cinbuf = std::cin.rdbuf(in.rdbuf());
+   std::ofstream out("out.txt");
+   auto coutbuf = std::cout.rdbuf(out.rdbuf());
 #endif
-   
+
    int numCasos;
    std::cin >> numCasos;
    for (int i = 0; i < numCasos; ++i)
       resuelveCaso();
-   
-   // para dejar todo como estaba al principio
+
+      // para dejar todo como estaba al principio
 #ifndef DOMJUDGE
    std::cin.rdbuf(cinbuf);
-   system("PAUSE");
+   std::cout.rdbuf(coutbuf);
 #endif
    return 0;
 }
